acf02aecde0a5bd067fae9888281ea66e1cceb82
ppaasssii <pascal.knoller@gmail.com>
2024-12-22 11:37:13 +0100

    Update index.js
    test 2 chat gpt connection

--- functions/index.js
+++ functions/index.js
@@ -130,7 +130,7 @@ function transformData(data) {
       odds: Object.keys(odds).map((oddKey) => {
         const odd = odds[oddKey];
         return {
-          betType: odd.betTypeID, // test1
+          betType: odd.betTypeID, // test2
           player: players[odd.playerID].name || "N/A",
           stat: odd.statID,
           overUnder: odd.overUnder,
